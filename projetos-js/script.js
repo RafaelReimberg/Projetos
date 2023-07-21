1238d96054fee01037f6729a0524c5425f3ab737
rafaelreimerbg <raaafael.reimberg@hotmail.com>
2023-07-21 09:37:54 -0300

    update

---
+++ projetos-js/script.js
@@ -0,0 +1,34 @@
+console.log("Rafael Marcelo Reimberg");
+
+document.write("<h1>Rafael Marcelo Reimberg!</h1>");
+
+var nome = "Rafael";
+var nome2 = "Reimberg"
+//var idade = 36;
+//var outraVariavel = true;
+var nomeFinal = nome + nome2;
+console.log(nomeFinal);
+document.write(`<strong>${nome2}</strong>`);
+
+var num1 = 20;
+var num2 = 25;
+var resultado = num1 + num2;
+
+document.write(resultado);
+
+if(num1 > num2){
+    
+    console.log("verdade");
+}else{
+    console.log("falso");
+}
+
+var frutas = ["laranja", "manga", "mamão", 30, false, true];
+ 
+
+//console.log(frutas[1]);
+
+
+for (var i = 0; i <= 10; i++){
+    console.log(i);
+}
